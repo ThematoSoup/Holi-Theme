0945fc21baab15a0b7512f1d97ba07d8fb991206
Slobodan Manic <slobodan.manic@gmail.com>
2013-08-23 23:57:34 +0200

    Deleted compressed JS file

--- js/theme-customizer-ck.js
+++
@@ -1,6 +0,0 @@
-/**
- * Theme Customizer enhancements for a better user experience.
- *
- * Contains handlers to make Theme Customizer preview reload changes asynchronously.
- * Things like site title and description changes.
- */(function(e){wp.customize("twentythirteen_scheme",function(t){t.bind(function(t){e("body").removeClass("holi-color-scheme-green holi-color-scheme-purple holi-color-scheme-pink holi-color-scheme-red holi-color-scheme-blue holi-color-scheme-yellow holi-color-scheme-turquoise holi-color-scheme-sepia holi-color-scheme-gray").addClass("holi-color-scheme-".to)})})})(jQuery);
